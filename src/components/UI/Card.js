bb494521abfafa9ffe43ccacd2832ddd5775a005
Okovic Ridvan <okovic.ridvan@gmail.com>
2022-10-31 00:15:03 +0100

    Css changes

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = (props) => {
   return (
-    <div className="container max-w-2xl mx-auto bg-white rounded-2xl shadow-xl font-mono border border-slate-200">
+    <div className="container max-w-2xl mx-auto bg-white  rounded-2xl shadow-xl font-mono border border-slate-200">
       {props.children}
     </div>
   );
